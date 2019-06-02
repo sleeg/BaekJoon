09989fdad144ebda4a03f5732e00a2fcf0863f19
sleeg <dlrhd19@gmail.com>
2019-06-03 01:04:13 +0900

    01 A+B

---
+++ AB.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class AB{
+   public static void main(String[] args) {
+      Scanner s = new Scanner(System.in);
+      double A = s.nextDouble();
+         System.out.println(A);
+   }
+
+}
---
+++ scanner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class scanner {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String A = s.nextLine();
+			System.out.println(A);
+	}
+
+}
