3a5480ac586d6a0333f46f901545aae4db702316
sleeg <dlrhd19@gmail.com>
2019-06-05 20:26:23 +0900

    test_file

---
+++ OP.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class OP{
+	public static void main(String[] arg){
+	Scanner scan = new Scanner(System.in);
+	int data = scan.nextInt();
+	int data2 = scan.nextInt();
+	System.out.println(data-data2);
+	}
+}
