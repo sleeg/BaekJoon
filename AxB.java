7a9185278524a026cfad403ae335cd2e69cf8db5
sleeg <dlrhd19@gmail.com>
2019-06-06 22:15:26 +0900

    AxB

---
+++ AxB.java
@@ -0,0 +1,16 @@
+import java.util.*; 
+public class AxB{
+ 
+ 	public static void main(String[] arg){
+ 
+ 	Scanner scan = new Scanner(System.in);
+ 
+ 	int a = scan.nextInt();
+ 
+ 	int b = scan.nextInt();
+ 
+ 	System.out.println(a*b);
+ 
+ 	} 
+
+ } 
