6f93cb204921b6c1b24dfcba58c3eb60ce1c37e3
sleeg <dlrhd19@gmail.com>
2019-06-08 16:44:33 +0900

    For01

---
+++ ForN.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class ForN{
+	public static void main(String[] arg){
+	Scanner scan = new Scanner(System.in);
+	int n = scan.nextInt();
+	for(int i=1; i<=n; i++)
+		System.out.println(i);
+	}
+}
